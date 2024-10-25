13916a1f5678270e8b99b20eaff4103761897034
Sruthy24 <sruthysmnglm@gmail.com>
2024-10-25 14:20:58 +0530

    Add files via upload

---
+++ largest no.py
@@ -0,0 +1,8 @@
+number1=int(input("enter the first number"))
+number2=int(input("enter the second number"))
+if number1>number2:
+    print(number1,"is greater than",number2)
+else:
+    print(number2,"is greater than",number1)
+    print("the remaining statement")
+
